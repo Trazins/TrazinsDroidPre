5b9d352b16e58781652cb28ad8b9295401964586
mctinez07 <mctinez07@gmail.com>
2021-05-27 21:13:41 +0200

    Update selectionActivity
    Create selection menu buttons

--- app/src/main/java/com/trazins/trazinsdroidpre/SelectionActivity.java
+++ app/src/main/java/com/trazins/trazinsdroidpre/SelectionActivity.java
@@ -2,20 +2,30 @@ package com.trazins.trazinsdroidpre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SelectionActivity extends AppCompatActivity {
 
     private TextView txtUserName;
+    private Button buttonLocate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
 
         txtUserName = (TextView)findViewById(R.id.textViewSelectionActivityUserName);
+        buttonLocate = (Button)findViewById(R.id.buttonLocateMenu);
 
         String userName = getIntent().getStringExtra("userName");
         txtUserName.setText(getString(R.string.identified_user) + " " + userName);
+    }
+
+    public void openSelectedActivity(View view){
+        Intent i = new Intent(getApplicationContext(),LocateActivity.class);
+        startActivity(i);
     }
 }
