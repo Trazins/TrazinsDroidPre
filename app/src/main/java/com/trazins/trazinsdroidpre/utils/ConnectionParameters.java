a8e386a4eaf06ab2c9207fbca09e09dbe7de7a5d
mctinez07 <mctinez07@gmail.com>
2022-02-21 14:55:53 +0100

    added salamanca connection string

--- app/src/main/java/com/trazins/trazinsdroidpre/utils/ConnectionParameters.java
+++ app/src/main/java/com/trazins/trazinsdroidpre/utils/ConnectionParameters.java
@@ -3,13 +3,14 @@ package com.trazins.trazinsdroidpre.utils;
 public class ConnectionParameters {
 
     //Selector de conexión
-    public static final int SET_URL_CONNECTION = 2;
+    public static final int SET_URL_CONNECTION = 3;
 
     //Listado de url de coenxión para los web service
     public static final String [] SOAP_ADDRESS = new String[]{
-            "http://188.165.209.37:8036/Android/TrazinsDroidService.svc", //Pruebas
-            "http://10.50.0.170:8006/Android/TrazinsDroidService.svc",
-            "http://188.165.209.37:8026/Android/TrazinsDroidService.svc", //Huesca
+            /*0*/ "http://188.165.209.37:8036/Android/TrazinsDroidService.svc", //Pruebas
+            /*1*/ "http://10.50.0.170:8006/Android/TrazinsDroidService.svc",
+            /*2*/ "http://188.165.209.37:8026/Android/TrazinsDroidService.svc", //Huesca
+            /*3*/ "http://10.100.0.234:8030/Android/TrazinsDroidService.svc" //Salamanca
     };
 
     //Datos de conexión del webservice
